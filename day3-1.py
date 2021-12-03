9f9ef38e59ae7f92d2166d1e9ed40acdf50c014a
Pete Mallam <pete.mallam@bjss.com>
2021-12-03 13:59:39 +0000

    Refactor day3 part 1

--- day3-1.py
+++ day3-1.py
@@ -1,28 +1,30 @@
 ## Advent of Code 2021
 ## Day 3 Part 1
-f = [l.rstrip() for l in open('day3.txt')]
-g = ["0"] * len(f[0])
-e = ["0"] * len(f[0])
-n = [0] * len(f[0])
-# Sum the values
-for x in f:
-    for b in range(0,len(f[0])):
-        n[b] = n[b] + int(x[b])
-print (n)
-# Parse the values
-for x in range(0,len(f)):
-    for v in range(0,len(f[0])):
-        if (n[v] > (int(len(f)/2))):
-            g[v] = "1"
-            e[v] = "0"
-        else:
-            g[v] = "0"
-            e[v] = "1"
-# Mux into proper Binary
-g = ''.join(g)
-e = ''.join(e)
-print ("Gamma   = " + g + " (" + str(int(g,2)) + ")")
-#print (int(g,2))
-print ("Epsilon = " + e + " (" + str(int(e,2)) + ")")
-#print (int(e,2))
-print ("Power Consumption = " + str(int(g,2) * int(e,2)))
+f = [l.rstrip() for l in open('day3-example.txt')]
+
+def calculatePower(method):
+    d = f
+    outputValue = ""
+    match method:
+        case "Epsilon":
+            defaultVal = "0"
+            useVal = "1"
+        case "Gamma":
+            defaultVal = "1"
+            useVal = "0"
+    for col in range(0,len(d[0])):
+        e = []
+        colCount = 0
+        for row in range(0,len(d)):
+            colCount = colCount + int(d[row][col])
+        colVal = defaultVal
+        if (colCount < (len(d)/2)):
+            colVal = useVal
+
+        outputValue = outputValue + colVal
+    print (method + " = " + str(outputValue))
+    return outputValue
+
+gamma  = calculatePower("Gamma")
+epsilon = calculatePower("Epsilon")
+print ("Power Consumption = " + str(int(gamma,2) * int(epsilon,2)))
