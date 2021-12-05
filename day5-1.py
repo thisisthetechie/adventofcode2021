49529cdc86bcbf995ccd2383ea208366991618db
Pete Mallam <pete.mallam@health.telstra.com>
2021-12-05 19:13:48 +0000

    Fix row/col

--- day5-1.py
+++ day5-1.py
@@ -17,13 +17,13 @@ for v in chartData:
         step = 1 if v[1]<v[3] else -1
         x = v[0]
         y = range(v[1],v[3]+step,step)
-        for row in y:
-            chartOut[row][x] += 1
+        for col in y:
+            chartOut[col][x] += 1
     elif (v[1] == v[3]):
         step = 1 if v[0]<v[2] else -1
         x = range(v[0],v[2]+step,step)
         y = v[1]
-        for col in x:
-            chartOut[y][col] += 1
+        for row in x:
+            chartOut[y][row] += 1
 print (chartOut)
 print ("Total number of overlaps:",numpy.count_nonzero(chartOut>1))
--- day5-2.py
+++ day5-2.py
@@ -17,14 +17,14 @@ for v in chartData:
         step = 1 if v[1]<v[3] else -1
         x = v[0]
         y = range(v[1],v[3]+step,step)
-        for row in y:
-            chartOut[row][x] += 1
+        for col in y:
+            chartOut[col][x] += 1
     elif (v[1] == v[3]):
         step = 1 if v[0]<v[2] else -1
         x = range(v[0],v[2]+step,step)
         y = v[1]
-        for col in x:
-            chartOut[y][col] += 1
+        for row in x:
+            chartOut[y][row] += 1
 
     else:
         stepX = 1 if v[0]<v[2] else -1
