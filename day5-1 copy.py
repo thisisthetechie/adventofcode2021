64beaea48ac62a67251eb04a5785ff948d19bddb
Pete Mallam <pete.mallam@health.telstra.com>
2022-12-03 12:48:43 +0000

    Day 5

--- day5-1 copy.py
+++ day5-1 copy.py
@@ -10,20 +10,19 @@ for v in f:
     tmp = re.split(',| -> ',v)
     chartData.append(tmp)
 chartData = numpy.array(chartData).astype(int)
-chartMax = chartData.max() + 1
+chartMax = chartData.max()+1
 chartOut = numpy.zeros((chartMax,chartMax),int)
 for v in chartData:
+    stepX = 1 if v[0]<v[2] else -1
+    stepY = 1 if v[1]<v[3] else -1
     if (v[0] == v[2]):
-        step = 1 if v[1]<v[3] else -1
-        x = v[0]
-        y = range(v[1],v[3]+step,step)
-        for col in y:
-            chartOut[col][x] += 1
+        stepY = 0
     elif (v[1] == v[3]):
-        
-        x = range(v[0],v[2]+step,step)
-        y = v[1]
-        for row in x:
-            chartOut[y][row] += 1
+        stepX = 0
+
+    for count in range(abs(v[0]-v[2])+1):
+            row = v[0]+(stepX * count)
+            col = v[1]+(stepY * count)
+            chartOut[col][row] += 1
 print (chartOut)
 print ("Total number of overlaps:",numpy.count_nonzero(chartOut>1))
--- day5-2.py
+++ day5-2.py
@@ -13,21 +13,19 @@ chartData = numpy.array(chartData).astype(int)
 chartMax = chartData.max()+1
 chartOut = numpy.zeros((chartMax,chartMax),int)
 for v in chartData:
+    stepX = 1 if v[0]<v[2] else -1
+    stepY = 1 if v[1]<v[3] else -1
     if (v[0] == v[2]):
-        step = 1 if v[1]<v[3] else -1
         x = v[0]
-        y = range(v[1],v[3]+step,step)
+        y = range(v[1],v[3]+stepY,stepY)
         for col in y:
             chartOut[col][x] += 1
     elif (v[1] == v[3]):
-        step = 1 if v[0]<v[2] else -1
-        x = range(v[0],v[2]+step,step)
+        x = range(v[0],v[2]+stepX,stepX)
         y = v[1]
         for row in x:
             chartOut[y][row] += 1
     else:
-        stepX = 1 if v[0]<v[2] else -1
-        stepY = 1 if v[1]<v[3] else -1
         for count in range(abs(v[0]-v[2])+1):
             row = v[0]+(stepX * count)
             col = v[1]+(stepY * count)
